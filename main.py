170c2e66e16410c97c3157166e6951fc5ccffc22
bileamScheuvens <benedictbileam@gmx.de>
2026-08-24 10:58:53 +0200

    sweep embed dim

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 from src.constants import CONFIGPATH
 import hydra
 from src.train import train_artsy
+
+
+# allow interpolation at resolve time
+OmegaConf.register_new_resolver("eval", eval)
 
 
 @hydra.main(version_base=None, config_path=str(CONFIGPATH), config_name="config")
