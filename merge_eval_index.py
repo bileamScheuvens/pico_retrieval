2451f946096964c719b6ca728cc0d26e39b6f288
bileamScheuvens <benedictbileam@gmx.de>
2026-09-02 12:48:44 +0200

    fix import

--- merge_eval_index.py
+++ merge_eval_index.py
@@ -1,12 +1,12 @@
 from omegaconf import DictConfig
 from src.constants import CONFIGPATH
 import hydra
-from src.data.indexing import eval_index_merge
+from src.data.indexing import eval_index_load
 
 
 @hydra.main(version_base=None, config_path=str(CONFIGPATH), config_name="eval")
 def merge_index(cfg: DictConfig):
-    eval_index_merge(cfg.index_name, k_shards=cfg.k_shards)
+    eval_index_load(cfg.index_name, k_shards=cfg.k_shards)
 
 
 if __name__ == "__main__":
